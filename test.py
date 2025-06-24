22dae654912a85aa165932477f15f58c059fa43a
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2025-06-24 15:03:21 +0530

    pandas rev

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello")
+import pandas as pd
+
+sales = {"user_id": ["HR51", "UP16", "25BH"], "Rto_": [143000, 148000, 35000]}
+
+sales_df = pd.DataFrame(sales)
+
+print(sales_df)
+print(sales_df.sort_values(by="Rto_", ascending=False))
